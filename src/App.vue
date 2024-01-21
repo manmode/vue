755b1a7f94b600511e102fd359f3b872ede713e0
Hiralal Manmode <hiralal.man@gmail.com>
2024-01-20 23:18:14 -0500

    init setup

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+let pageBackground = ref('#00000000');
+let arrLetters = ref([
+  'a','b','c'
+]);
+function changeColor() {
+  pageBackground.value = '#' + Math.floor(Math.random() * 10000000).toString(16);
+}
+</script>
+
+<template>
+  <div id="app" class="app">
+    {{ appName }}
+    {{ pageBackground }}
+
+    <button @click="changeColor()">Change Color</button>
+    <div>
+      <p>Message is: {{ pageBackground }}</p>
+      <input v-model="pageBackground" placeholder="edit me" />
+    </div>
+
+    <div>
+      arrLetters.map((item) => {
+          {{ item }}  
+        })
+    </div>
+</div>
+</template>
+
+<style scoped>
+.app {
+  font-size: 30px;
+}
+</style>
